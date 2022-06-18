ed0caa456f5c70ee9ea200f001fcd6b87d510036
NzninMrdn <nazani16mrdn@gmail.com>
2022-06-18 16:17:01 +0430

    create project

---
+++ articles/views.py
@@ -0,0 +1,12 @@
+from msilib.schema import LockPermissions
+from django.shortcuts import render
+from . import models
+
+
+# Create your views here.
+def articles_list(request):
+    articles = models.Articles.objects.all().order_by('date')
+    args = {"articles":articles}
+    return render(request, 'articles/articleslist.html', args)
+
+
